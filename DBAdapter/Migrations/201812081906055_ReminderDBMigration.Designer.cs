1f8559ef0f9f463dfd31db1f4de60c762d97acde
LCherepina <cherepinaliza@gmail.com>
2018-12-08 21:43:27 +0200

    .

--- DBAdapter/Migrations/201812081906055_ReminderDBMigration.Designer.cs
+++ DBAdapter/Migrations/201812081906055_ReminderDBMigration.Designer.cs
@@ -13,7 +13,7 @@ namespace Architecture_Reminder.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201812051807334_ReminderDBMigration"; }
+            get { return "201812081906055_ReminderDBMigration"; }
         }
         
         string IMigrationMetadata.Source
